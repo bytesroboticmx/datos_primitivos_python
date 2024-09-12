1f6b691f5ae81330330b7a3d6fc87487b988c9a2
bytesroboticmx <120799721+bytesroboticmx@users.noreply.github.com>
2024-09-11 23:50:31 -0600

    Version 0.1

---
+++ datos_primitivos.py
@@ -0,0 +1,19 @@
+# Tipo de dato entero (int)
+numero_entero = 42
+print(f"El número entero es {numero_entero}, que es del tipo {type(numero_entero)}")
+
+# Tipo de dato flotante (float)
+numero_flotante = 3.1416
+print(f"El número flotante es {numero_flotante}, que es del tipo {type(numero_flotante)}")
+
+# Tipo de dato booleano (bool)
+valor_booleano = True
+print(f"El valor booleano es {valor_booleano}, que es del tipo {type(valor_booleano)}")
+
+# Tipo de dato cadena de texto (str)
+cadena_texto = "Hola, Mundo!"
+print(f"La cadena de texto es '{cadena_texto}', que es del tipo {type(cadena_texto)}")
+
+# Tipo de dato bytes (bytes)
+dato_bytes = b'Hola'
+print(f"El valor en bytes es {dato_bytes}, que es del tipo {type(dato_bytes)}")
